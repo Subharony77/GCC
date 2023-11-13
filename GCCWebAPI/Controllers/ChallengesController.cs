10c1cece185be72dc76bcec81ad31c5168ef1367
Subhakanta Sahoo <subha.rony77@gmail.com>
2023-11-13 11:31:12 +0530

    Initial Push

---
+++ GCCWebAPI/Controllers/ChallengesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCCWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChallengesController : ControllerBase
+    {
+
+    }
+}
